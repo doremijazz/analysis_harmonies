bd431585ca7f3793fb97c68f46df3e920fa9392c
doremijazz <55767732+doremijazz@users.noreply.github.com>
2023-06-10 16:02:05 +0200

    Update main.py

--- programme/main.py
+++ programme/main.py
@@ -1,5 +1,5 @@
 ##### Main analysis harmonie MXL score ####
-print("code de github, je vais m'exécuter)
+print("code de github, je vais m'exécuter")
 from music21 import *
 from tkinter.filedialog import *
 from tkinter import *
