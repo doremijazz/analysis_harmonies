9595cb5965219f79daa2338d92af0325a8364517
doremijazz <55767732+doremijazz@users.noreply.github.com>
2023-06-22 00:16:46 +0200

    Add files via upload

---
+++ analyse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Hello World!"
+
+python3.11 -m venv myenv
+source myenv/bin/activate
+
+pip3 install music21 
+
+echo "Total Number of Arguments:" $#
+
+echo "Argument values:" $@
+
+cd ~/Downloads/analysis_harmonies/programme/
+python main.py $@
+
+echo "finish"
